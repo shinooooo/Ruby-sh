86b14cac7b46846de4769e7237bfca3a419cd009
shinooooo <bjys1290150@gn.iwasaki.ac.jp>
2020-12-06 01:44:22 +0900

    ls -lのst_modeパース実装

--- ls.rb
+++ ls.rb
@@ -14,14 +14,12 @@ class List
     options = get_arg
     dirs = options.count != 0 ? validate(options) : ["."]
     multi_flag =  dirs.count > 2 ? true : false
-
     dirs.each do |dir|
-      display(dir,multi_flag)
+      display(dir, multi_flag)
     end
   end
   
   private
-
   def get_arg
     ARGV
   end
@@ -40,7 +38,7 @@ class List
 
   def display(dir,multi_flag)
     printf("%s:\n", dir) if multi_flag
-    if @options[:list]
+    if @options[:long]
       display_list(dir)
     else
       display_common(dir)
@@ -48,17 +46,53 @@ class List
   end
 
   def display_list(dir)
+    files = Dir.children(dir).filter{ |file| file[0] != "." }
+    # printf("total %s\n", );
+    files.each do |file|
+      case File.ftype(file)
+      when "file"
+        stmode = "-"
+      when "directory"
+        stmode = "d"
+      when "characterSpecial"
+        stmode = "c"
+      when "blockSpecial"
+        stmode = "b"
+      when "fifo"
+        stmode = "p"
+      when "link"
+        stmode = "l"
+      when "socket"
+        stmode = "s"
+      else
+        stmode = "?"
+      end
+
+      permission = ("%#b" % File.stat(file).world_readable?).delete_prefix("0b")
+      permission = permission.chars
+      p permission
+      (0..2).each {
+        stmode.concat(permission.shift == '1' ? "r" : "-")
+        stmode.concat(permission.shift == '1' ? "w" : "-")
+        stmode.concat(permission.shift == '1' ? "x" : "-")
+      }
+      p stmode
+
+    end
+
   end
 
   def display_common(dir) 
-    files =  @options[:all] ? Dir.entries(dir) : Dir.entries(dir).filter{ |file|  file[0] != "." }
-
+    files =  @options[:all] ? Dir.entries(dir) : Dir.children(dir).filter{ |file| file[0] != "." }
     files.each do |file|
       if File.directory?(file)
         printf("\e[#{@@dir_color}m%-10s\e[0m\t", file)
-      else 
+      elsif File.symlink?(file)
+        printf("\e[45m%-10s\e[0m\t", file)
+      else
         printf("%-10s\t", file)
       end
+      p File.ftype(file)
     end
     print("\n")
   end
@@ -66,3 +100,4 @@ end
 
 list = List.new
 list.exec
+
